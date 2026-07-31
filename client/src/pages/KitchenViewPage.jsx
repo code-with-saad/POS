0707cbb3f3fcbd2cacecbf87299f59dc9c7737ae
code-with-saad <saadkashif770@gmail.com>
2026-07-31 21:57:17 +0500

    Fix JSX syntax in KitchenViewPage

--- client/src/pages/KitchenViewPage.jsx
+++ client/src/pages/KitchenViewPage.jsx
@@ -219,7 +219,7 @@ export default function KitchenViewPage() {
                                 </span>
                               )}
                             </div>
-                          // </div>
+                          </div>
                         );
                       });
                     })()}
